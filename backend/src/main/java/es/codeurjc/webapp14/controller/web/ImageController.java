81e9c41abaff7d76a9969502066b649b8d94aaf9
jorgeramirezgayo <jorgeramirezgayo@outlook.es>
2025-03-23 17:13:42 +0100

    Updated ImageController

--- backend/src/main/java/es/codeurjc/webapp14/controller/web/ImageController.java
+++ backend/src/main/java/es/codeurjc/webapp14/controller/web/ImageController.java
@@ -1,22 +1,18 @@
 
 package es.codeurjc.webapp14.controller.web;
 
-import java.sql.Blob;
 import java.sql.SQLException;
-import java.util.Optional;
 
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import es.codeurjc.webapp14.model.Product;
 import es.codeurjc.webapp14.service.ProductService;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/image")
@@ -28,31 +24,14 @@ public class ImageController {
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity<byte[]> getProductImage(@PathVariable Long id) {
-        Optional<Product> existproduct = productService.getProductById(id);
+    public ResponseEntity<Object> getProductImage(@PathVariable long id) throws SQLException, IOException {
 
-        Product product = existproduct.get();
+		Resource productImage = productService.getProductImage(id);
 
-        if (product == null) {
-            return ResponseEntity.notFound().build();
-        }
+		return ResponseEntity
+				.ok()
+				.header(HttpHeaders.CONTENT_TYPE)
+				.body(productImage);
 
-        if (product.getImage() == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        try {
-            Blob imageBlob = product.getImage();
-            byte[] imageBytes = imageBlob.getBytes(1, (int) imageBlob.length());
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG);
-
-            return ResponseEntity.ok().headers(headers).body(imageBytes);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
+	}
 }
