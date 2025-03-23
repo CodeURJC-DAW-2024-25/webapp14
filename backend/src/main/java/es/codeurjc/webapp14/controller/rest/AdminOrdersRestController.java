b77f8d363120496d1f991a2b7f36eb6d01ba62af
IciarML <i.morenolo.2022@alumnos.urjc.es>
2025-03-23 18:48:34 +0100

    Added AdminOrdersRestController

--- backend/src/main/java/es/codeurjc/webapp14/controller/rest/AdminOrdersRestController.java
+++ backend/src/main/java/es/codeurjc/webapp14/controller/rest/AdminOrdersRestController.java
@@ -1,7 +1,6 @@
 package es.codeurjc.webapp14.controller.rest;
 
 import es.codeurjc.webapp14.service.OrderService;
-import es.codeurjc.webapp14.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,36 +15,18 @@ public class AdminOrdersRestController {
     @Autowired
     private OrderService orderService;
 
-    @Autowired
-    private OrderMapper orderMapper;
-
     @GetMapping
     public ResponseEntity<Map<String, Object>> getAdminOrders(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
         Map<String, Object> data = new HashMap<>();
 
-        // Get paginated orders
-        data.put("orders", orderService.getOrdersPaginated(page, size)
-                .getContent()
-                .stream()
-                .map(orderMapper::toDTO)
-                .toList());
-
-        // Get total pages
-        data.put("totalPages", orderService.getOrdersPaginated(page, size).getTotalPages());
-
-        // Get total sales
-        data.put("totalSales", orderService.getTotalSales());
-
-        // Get today's sales
-        data.put("todaySales", orderService.getTodaySales());
-
         // Get total orders
         data.put("totalOrders", orderService.getTotalOrders());
 
-        // Get orders last 30 days
-        data.put("ordersLast30Days", orderService.getOrdersLast30Days());
+        // Get paginated orders
+        // Refactor? products paginated has hasMore and nextPage attributes?
+        data.put("orders", orderService.getOrdersPaginated(page, size));
 
         return ResponseEntity.ok(data);
     }
