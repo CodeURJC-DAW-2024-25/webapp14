da2489e680cc1d36f7a8c5350e9ec59f11b326f2
IciarML <i.morenolo.2022@alumnos.urjc.es>
2025-03-24 14:36:53 +0100

    Added Docker setup including Dockerfile, create_image script, and docker-compose configuration

---
+++ backend/docker/create_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ..
+docker build -t webapp14:latest -f docker/Dockerfile . 
---
+++ backend/docker/publish_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker tag webapp14:latest iciar04/webapp14:latest
+docker push iciar04/webapp14:latest 
