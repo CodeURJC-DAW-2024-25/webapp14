9e20193693c875102a863cc19f2ff3f2ea44149c
IciarML <i.morenolo.2022@alumnos.urjc.es>
2025-04-25 12:04:22 +0200

    Added admin orders

--- frontend/src/app/admin/orders/orders.component.ts
+++ frontend/src/app/admin/orders/orders.component.ts
@@ -1,9 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../../services/order.service';
+import { OrderDTO } from '../../dtos/order.dto';
+
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html'
 })
-export class OrdersAdminComponent {
+export class OrdersAdminComponent implements OnInit {
+
+  orders: { id: number, [key: string]: any }[] = [];
+
+  orderCount: number = 0;
+  loading: boolean = false;
+  noOrders: boolean = false;
+
+  currentPage: number = 0;
+  totalPages: number = 0;
+  pageSize: number = 10;
+
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit(): void {
+    this.loadOrders();
+  }
+
+  loadOrders(): void {
+    this.loading = true;
+    this.orderService.getAllOrders(this.currentPage, this.pageSize).subscribe({
+      next: (data) => {
+        this.totalPages = data.orders.totalPages;
+        this.orders = data.orders.content.map((order: OrderDTO) => ({
+          ...order,
+          showDetails: false
+        }));
+        this.orderCount = data.totalOrders;
+        this.loading = false;
+        this.noOrders = this.orders.length === 0;
+      },
+      error: (err) => {
+        console.error('Error al cargar los pedidos:', err);
+        this.loading = false;
+      }
+    });
+  }
+
+  deleteOrder(orderId: number): void {
+    this.orderService.deleteOrder(orderId).subscribe(() => {
+      this.orders = this.orders.filter(order => order.id !== orderId);
+      this.loadOrders();
+    });
+  }
+
+  updateOrderState(order: any): void {
+    this.orderService.updateOrderState(order.id, order.state).subscribe(() => {
+      console.log('order.state');
+      console.log('Estado del pedido actualizado');
+    });
+  }
+
+  loadMoreOrders(): void {
+    this.currentPage++;
+    this.loading = true;
+    this.orderService.getAllOrders(this.currentPage, this.pageSize).subscribe({
+      next: (data) => {
+        const newOrders = data.orders.content;
+        this.orders = this.orders.concat(newOrders);
+        this.totalPages = data.orders.totalPages;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error al cargar más pedidos:', err);
+        this.loading = false;
+      }
+    });
+  }
 
 }
--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 import { environment } from '../environment';
 
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class OrderService {
 
     private baseUrl = `${environment.apiUrl}/users/orders`;
 
-    constructor(private http: HttpClient) {}
+    private url = `${environment.apiUrl}/orders`;
 
+    constructor(private http: HttpClient) { }
 
     getOrders(userId: number): Observable<any> {
         return this.http.get<any>(`${this.baseUrl}?userId=${userId}`);
@@ -21,5 +22,18 @@ export class OrderService {
     getOrder(userId: number, orderId: number): Observable<any> {
         return this.http.get<any>(`${this.baseUrl}/${orderId}?userId=${userId}`);
     }
-      
+
+    getAllOrders(page: number, pageSize: number): Observable<any> {
+        const url = `${environment.apiUrl}/orders?page=${page}&size=${pageSize}`;
+        return this.http.get<any>(url);
+    }
+
+    deleteOrder(orderId: number): Observable<any> {
+        return this.http.delete(`${this.url}/${orderId}`);
+    }
+
+    updateOrderState(orderId: number, newState: string): Observable<any> {
+        return this.http.patch(`${this.url}/${orderId}?newState=${newState}`, {});
+    }
+
 }
