bbfaaecfc21fe5dcec2a2e82417e92922eb7e691
jorgeramirezgayo <jorgeramirezgayo@outlook.es>
2025-04-26 20:30:56 +0200

    Fixed some styles

--- frontend/src/app/user/index/index.component.ts
+++ frontend/src/app/user/index/index.component.ts
@@ -17,43 +17,43 @@ export class IndexComponent implements OnInit {
   loading: boolean = false;
 
   categoryBlocks = [
-  { name: 'PANTALONES', image: 'assets/images/Pantalones.webp' },
-  { name: 'CAMISETAS', image: 'assets/images/chaquetas.webp' },
-  { name: 'ABRIGOS', image: 'assets/images/ABRIGO.webp' },
-  { name: 'JERSEYS', image: 'assets/images/LANDING-punto.webp' }
-];
+    { name: 'PANTALONES', image: 'assets/images/Pantalones.webp' },
+    { name: 'CAMISETAS', image: 'assets/images/chaquetas.webp' },
+    { name: 'ABRIGOS', image: 'assets/images/ABRIGO.webp' },
+    { name: 'JERSEYS', image: 'assets/images/LANDING-punto.webp' }
+  ];
 
-services = [
-  {
-    title: 'Envíos Gratis',
-    description: 'En toda la península Ibérica',
-    icon: 'ci:shopping-cart-02'
-  },
-  {
-    title: 'Pago Seguro 100%',
-    description: 'Tus compras están protegidas con métodos cifrados.',
-    icon: 'tdesign:secured'
-  },
-  {
-    title: 'Garantía de Calidad',
-    description: 'Productos con los más altos estándares de calidad.',
-    icon: 'la:award'
-  },
-  {
-    title: 'Ofertas Diarias',
-    description: 'Descubre descuentos exclusivos y promociones.',
-    icon: 'solar:dollar-outline'
-  }
-];
+  services = [
+    {
+      title: 'Envíos Gratis',
+      description: 'En toda la península Ibérica',
+      icon: 'ci:shopping-cart-02'
+    },
+    {
+      title: 'Pago Seguro 100%',
+      description: 'Tus compras están protegidas con métodos cifrados.',
+      icon: 'tdesign:secured'
+    },
+    {
+      title: 'Garantía de Calidad',
+      description: 'Productos con los más altos estándares de calidad.',
+      icon: 'la:award'
+    },
+    {
+      title: 'Ofertas Diarias',
+      description: 'Descubre descuentos exclusivos y promociones.',
+      icon: 'solar:dollar-outline'
+    }
+  ];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.loadProducts();
   }
 
   loadProducts(): void {
-    this.productService.getIndexProducts(0,4).subscribe({
+    this.productService.getIndexProducts(0, 4).subscribe({
       next: data => {
         this.products = data.bestSellingProducts ?? [];
       },
@@ -64,16 +64,12 @@ services = [
   loadMoreRecommendedProducts(): void {
     this.page++;
     this.productService.getProducts(this.page, this.size).subscribe({
-    next: data => {
-      this.products.push(...data.products.content);
-    },
-    error: err => {
-      console.error('Error al cargar más recomendados:', err);
-    }
-  });
-}
-
-
-  
-
+      next: data => {
+        this.products.push(...data.products.content);
+      },
+      error: err => {
+        console.error('Error al cargar más recomendados:', err);
+      }
+    });
+  }
 }
